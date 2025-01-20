7e9f133dfa3d9699478790e2b7fea626fb3e0779
Luxary <f.leiton001@gmail.com>
2025-01-20 13:10:53 -0300

    fix-formato json, query post

--- models/products.model.js
+++ models/products.model.js
@@ -20,7 +20,7 @@ export class ProductsModel {
       console.log(product);
       const { id, nombre, marca, precio, stock } = product;
       const result = await pool.query(
-        "INSERT INTO products (product_id ,product_name, product_brand, product_price, product_stock) VALUES ($1, $2, $3, $4, $5) ON CONFLICT (product_id) DO UPDATE SET product_stock = (SELECT product_stock + $5 FROM products) RETURNING product_id, product_name, product_brand, product_price, product_stock",
+        "INSERT INTO products (product_id ,product_name, product_brand, product_price, product_stock) VALUES ($1, $2, $3, $4, $5) ON CONFLICT (product_id) DO UPDATE SET product_stock = (SELECT product_stock + $5 FROM products WHERE product_id = $1) RETURNING product_id, product_name, product_brand, product_price, product_stock",
         [id, nombre, marca, precio, stock]
       );
       return result;
